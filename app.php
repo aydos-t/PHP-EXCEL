6e8a0edb8e9cc7c2619382f941dee7a598579bc0
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2023-09-15 17:09:02 +0500

    specify file type

--- app.php
+++ app.php
@@ -2,5 +2,7 @@
 if (isset($_POST['save_excel_data'])) {
     $fileName = $_FILES['import_file']['name'];
     $fileExt = pathinfo($fileName, PATHINFO_EXTENSION);
+
+    $allowedExt = ['xls', 'csv', 'xlsx'];
 }
 ?>
