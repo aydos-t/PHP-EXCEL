2e7bbf09648a00a0db2aa0c281e76f56bf02a46c
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2023-09-15 17:13:28 +0500

    package connection

--- app.php
+++ app.php
@@ -1,8 +1,19 @@
 <?php
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 if (isset($_POST['save_excel_data'])) {
     $fileName = $_FILES['import_file']['name'];
     $fileExt = pathinfo($fileName, PATHINFO_EXTENSION);
 
     $allowedExt = ['xls', 'csv', 'xlsx'];
+
+    if (in_array($fileExt, $allowedExt)) {
+        $inputFileNamePath = $_FILES['import_file']['tmp_name'];
+        $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory ::load($inputFileNamePath);
+        $data = $spreadsheet -> getActiveSheet() -> toArray();
+    }
 }
 ?>
